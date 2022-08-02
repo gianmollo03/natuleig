b409d206f8cf042c8bf23a1b8b77301ea5b45396
gianmollo03 <gianmollo03@gmail.com>
2022-08-02 01:52:12 -0300

    Añado estilos a los botones

--- natuleig_app/src/componentes/ItemCount.js
+++ natuleig_app/src/componentes/ItemCount.js
@@ -2,23 +2,36 @@ import React from "react";
 import { useState } from "react";
 
 const ItemCount =()=>{
+
+
+
     const resultado = useState(0)
     let contador = resultado[0]
     const setContador=resultado[1]
+
+
+
     const aumentarContador=()=>{
         console.log("Aumentar contador")
         setContador(contador+1)
     }
+
+
     const disminuirContador=()=>{
         console.log("Disminuir contador")
         setContador(contador-1)
     }
     
     return (
-        <div className="grupoBotonContador">
+        <div className="grupoBotonesCarritoContainer">
+            <div className="botonesContadorContainer">
             <button onClick={disminuirContador}>-1</button>
             <p>{contador}</p>
             <button onClick={aumentarContador}>+1</button>
+            </div>
+            <div className="botonCarritoContainer">
+                <button id="botonAñadirCarrito">Añadir al carrito</button>
+            </div>
         </div>
     )
 }
