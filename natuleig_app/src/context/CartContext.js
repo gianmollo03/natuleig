f8315e957c906d747baf67d4b8463ecda2a988ed
gianmollo03 <gianmollo03@gmail.com>
2022-08-25 13:19:21 -0300

    Arregloel cartcontext

--- natuleig_app/src/context/CartContext.js
+++ natuleig_app/src/context/CartContext.js
@@ -7,19 +7,44 @@ export function CartContextProvider ({ children }) {
         
         const   isInCart = (id) => cartList.find(prod => prod.id == id)
 
-        const addToCart=(item,quantity)=>{
+        const addToCart=(item,cantidad)=>{
             if(isInCart(item.id)){  
-                const newCart = cartList.map(prod=>{
-                    const newQuantity = prod.quantity + quantity;
-                    return {...prod,quantity: newQuantity} 
-                })
+                const newCart  = cartList.map(prod=>{
+                    if(prod.id === item.id){
+                        const newCantidad = prod.cantidad + cantidad;
+                        return {...prod,cantidad: newCantidad}  } 
+                    else{
+                        return prod
+                    }
+                    })
+                    setCartList(newCart)
+                    }
+            else{
+                const newProduct = {...item,cantidad:cantidad}
+                setCartList([...cartList,newProduct])
             }
-        }
-    return(
-        <CartContext.Provider value = {{
 
-        }}>
-            { children }
+            }
+        const removeProduct = (id) => setCartList(cartList.filter(prod=>prod.id != id ))
+        const cleanCart = () => setCartList([])
+        const totalPrecio = () =>{
+            return cartList.reduce ((acc,product)=> acc +=(product.precio*product.cantidad),0)     
+
+        }
+        const totalCantidad = () =>{
+            return cartList.reduce((add,product) => acc += product.cantidad,0)
+        }
+        return(
+            <CartContext.Provider value={{
+                addToCart,
+                removeProduct,
+                cleanCart,
+                totalPrecio,
+                totalCantidad
+            }}>
+                { children }
             </CartContext.Provider>
+        
+        
     )
-}
+        }
