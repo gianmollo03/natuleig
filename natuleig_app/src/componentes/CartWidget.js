138d392b315df95a0f7a82f7fc1285cf38146452
gianmollo03 <gianmollo03@gmail.com>
2022-08-25 23:14:13 -0300

    Arranco la estructura del cartview

--- natuleig_app/src/componentes/CartWidget.js
+++ natuleig_app/src/componentes/CartWidget.js
@@ -1,18 +1,15 @@
-import ShoppingCart from "@mui/icons-material/ShoppingCart"
-import CartIcon from "@mui/icons-material/ShoppingCart"
-import { Badge } from "@mui/material"
-// import { useContext } from "react"
-// import { CartContext } from "../context/CartContext"
+import { Icon } from "@chakra-ui/react";
+import ShoppingCart from "@mui/icons-material/ShoppingCart";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
-
-export const CartWidget = () =>{
-    // const {conocerCantidad} = useContext(CartContext)
-    
+const CartWidget = () =>{
+    const {obtenerCantidad} = useContext(CartContext)
     return(
-        <div>
-            <ShoppingCart></ShoppingCart>
-            {/* <p>{conocerCantidad()}</p> */}
+        <div className="CartWidget">
+            <Icon as={ShoppingCart}></Icon>
+            <p>{obtenerCantidad()}</p>
         </div>
     )
 }
-export default CartWidget
+export default CartWidget 
