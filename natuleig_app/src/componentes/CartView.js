2f7067b9bfbe94e64b476214064632c768a21466
gianmollo03 <gianmollo03@gmail.com>
2022-08-30 18:51:46 -0300

    Intento de CartView

--- natuleig_app/src/componentes/CartView.js
+++ natuleig_app/src/componentes/CartView.js
@@ -1,29 +1,25 @@
 
-import { Box, Input, Text } from "@chakra-ui/react"
+import { Box, Center, HStack, Image, Input, Text, VStack } from "@chakra-ui/react"
 import {  useCarrito } from "../context/CartContext"
-
+import React, { useContext } from "react"
 
 
 export const CartView =()=>{
-    const {carrito,eliminarItemCarrito,limpiarCarrito,obtenerPrecioTotal} = useCarrito()
+    const {carrito} = useCarrito()
+    console.log(carrito)
     return(
-        <>
-        <div>
-            <h1>Carrito</h1>
-            <h2>Productos:</h2>
-            )
-           { carrito.map(prod=>{
-                    <div>
-                    <p>{prod.nombre}</p>
-                    <p>{prod.cantidad}</p>
-                    <p>
-                        {prod.precio}
-                    </p>
-                    <img src={prod.image} alt="" />
-                    
-                    </div>})}
-        </div>
-        </>
+        <Center>
+            <VStack>
+                {carrito.map(prod=>{
+                    <HStack key={prod.id} boxShadow='lg' p='6' rounded='md' spacing={10} bg='white'>
+                        <Image src={prod.image} w='100px'></Image>
+                        <Text as='b'>{prod.nombre}</Text>
+                        <Text as='b'>{prod.precio}</Text>
+                        <Text as='b'>{prod.cantidad}</Text>
+                    </HStack>
+                })}
+            </VStack>
+        </Center>
     )
 
 }
--- natuleig_app/src/componentes/CartWidget.js
+++ natuleig_app/src/componentes/CartWidget.js
@@ -1,5 +1,4 @@
 import { Icon } from "@chakra-ui/react";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useContext } from "react";
 import { CartContext, useCarrito } from "../context/CartContext";
 import { Link } from "react-router-dom";
@@ -8,7 +7,7 @@ const CartWidget = () =>{
     const {obtenerCantidad} = useCarrito()
     return(
         <div className="CartWidget">
-            <Icon as={ShoppingCartIcon}></Icon> 
+            
             <p>{obtenerCantidad()}</p>
         </div>
     )
