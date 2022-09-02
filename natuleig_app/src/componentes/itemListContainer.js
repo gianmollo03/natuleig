ce5ff073e4c80c5863a24023d784dd2bbda62ae5
gianmollo03 <gianmollo03@gmail.com>
2022-09-02 19:47:11 -0300

    falta arreglar item collector

--- natuleig_app/src/componentes/itemListContainer.js
+++ natuleig_app/src/componentes/itemListContainer.js
@@ -3,36 +3,43 @@ import { productos } from "../utiles/productos";
 import { CustomFetch } from "../utiles/customFetch";
 import { useEffect, useState } from "react";
 import { ItemList } from "./ItemList";
-import { Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { PageTemplate } from "./Page";
+import database from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+const productosCollection = collection(database, "productos");
 
 const ItemListContainer = (greeting) => {
   const [listaProductos, setListaProductos] = useState([]);
-  const [loading, setLoading] = useState(true);
   const { categoria } = useParams();
   useEffect(() => {
-    setLoading(true);
-    CustomFetch(productos).then((res) => {
-      if (categoria) {
-        setLoading(false);
-        setListaProductos(res.filter((prod) => prod.categoria === categoria));
-      } else {
-        setLoading(false);
-        setListaProductos(res);
-      }
-    });
+    const consulta = getDocs(productosCollection);
+    console.log(consulta);
+    consulta
+      .then((snapshot) => {
+        const documentos = snapshot.docs;
+        documentos.map((producto) => {
+          return listaProductos.push(producto.data());
+        });
+        console.log(listaProductos);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    if (categoria) {
+      setListaProductos(
+        listaProductos.filter((producto) => producto.categoria === categoria)
+      );
+    } else {
+      setListaProductos(listaProductos);
+    }
   }, [categoria]);
-
   return (
     <div id="listaDeItems">
-      {!loading ? (
-        <PageTemplate titulo="Productos" subtitulo={categoria}>
-          <ItemList listaProductos={listaProductos} />
-        </PageTemplate>
-      ) : (
-        <Text>Cargando...</Text>
-      )}
+      <PageTemplate titulo="Productos" subtitulo={categoria}>
+        <ItemList listaProductos={listaProductos} />
+      </PageTemplate>
     </div>
   );
 };
