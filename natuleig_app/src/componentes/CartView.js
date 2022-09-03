952f12b9eee139d60539b4af4eaa63b594332f8e
gianmollo03 <gianmollo03@gmail.com>
2022-09-03 09:03:31 -0300

    cambio la estructura del carrito

--- natuleig_app/src/componentes/CartView.js
+++ natuleig_app/src/componentes/CartView.js
@@ -1,4 +1,20 @@
-import { Center, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Center,
+  Divider,
+  HStack,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
 import { useCarrito } from "../context/CartContext";
 
 export const CartView = () => {
@@ -6,26 +22,79 @@ export const CartView = () => {
   console.log(carrito);
   return (
     <VStack>
-      <Center>
-        {carrito.map((prod) => {
-          return (
-            <>
-              <HStack
+      <Center className="carritoContenedor">
+        <TableContainer>
+          <Table>
+            <TableCaption>Carrito de compras</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Imagen</Th>
+                <Th>Nombre</Th>
+                <Th>Precio unitario</Th>
+                <Th>Cantidad</Th>
+                <Th>Precio final</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {carrito.map((prod) => {
+                return (
+                  <>
+                    {/* <HStack
                 key={prod.id}
                 boxShadow="lg"
                 p="6"
                 rounded="md"
                 spacing={10}
-                bg="white"
+                className="productoCarritoContenedor"
               >
-                <Image src={prod.image} w="100px"></Image>
-                <Text as="b">{prod.nombre}</Text>
-                <Text as="b">{prod.cantidad}</Text>
-                <Text as="b">${prod.precio}</Text>
-              </HStack>
-            </>
-          );
-        })}
+                <div className="cartProductImage">
+                  <Text as="b">Imagen</Text>
+                  <Divider></Divider>
+                  <Image src={prod.image} w="100px"></Image>
+                </div>
+                <div className="cartProductName">
+                  <Text as="b">Nombre</Text>
+                  <Divider></Divider>
+                  <Text as="b">{prod.nombre}</Text>
+                </div>
+                <div className="cartProductQuantity">
+                  <Text as="b">Cantidad</Text>
+                  <Divider></Divider>
+                  <Text as="b">{prod.cantidad}</Text>
+                </div>
+                <div className="cartProductPrice">
+                  <Text as="b">Precio unitario</Text>
+                  <Divider></Divider>
+                  <Text as="b">${prod.precio}</Text>
+                </div>
+                <div className="cartProductTotal">
+                  <Text as="b">Precio final</Text>
+                  <Text as="b">${prod.precio * prod.cantidad}</Text>
+                </div>
+              </HStack> */}
+                    <Tr>
+                      <Td>
+                        <Image src={prod.image} width="100px"></Image>
+                      </Td>
+                      <Td>
+                        <Text as="b">{prod.nombre}</Text>
+                      </Td>
+                      <Td>
+                        <Text as="b">${prod.precio}</Text>
+                      </Td>
+                      <Td>
+                        <Text as="b">{prod.cantidad}</Text>
+                      </Td>
+                      <Td>
+                        <Text as="b">${prod.precio * prod.cantidad}</Text>
+                      </Td>
+                    </Tr>
+                  </>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Center>
     </VStack>
   );
